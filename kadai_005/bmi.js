f5b88934769913f2755b98a241f9a36d48043b2b
gaon2002 <gaon2002@outlook.jp>
2024-02-14 09:30:49 +0900

    kadai_005>bmi.jp変更：BMI変数の追加

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,6 +1,10 @@
 // 変数の宣言と値の代入
+// 体重
 let weight = 68;
+// 身長
 let height = 1.7;
+// BMI
+let bmi = (weight / (height * height));
 
 // 結果表示
-console.log(weight / (height * height))
+console.log(bmi);
