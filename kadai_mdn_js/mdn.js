715e7041c658f463d1e9d1530a4fd377b520ae26
gaon2002 <gaon2002@outlook.jp>
2024-02-15 14:16:53 +0900

    課題の作成２０２４年２月１５日　MDN

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,16 @@
+// 今日の日付を取得し、表示させる
+
+
+// JavaScriptのDate()関数において、newをつけるとDateオブジェクトのインスタンスが作成される。
+const today = new Date();
+
+// 年月日それぞれのデータを定義
+const year = today.getFullYear();
+const month = today.getMonth() + 1;
+const date = today.getDate();
+
+// 年月日を出力
+// シングルorダブルクオーテーションではなく、バッククオートを使う
+console.log(`${ year }年${ month }月${ date }日`);
+// もしくは+記号を使う
+console.log(year + '年' + month + '月' + date + '日');
