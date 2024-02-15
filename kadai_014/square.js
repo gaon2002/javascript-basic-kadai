4a785eeebb1231b931ab2c7b88e8bad34c2ca60c
gaon2002 <gaon2002@outlook.jp>
2024-02-15 17:45:59 +0900

    JavaScriptの基礎を学ぼう_14章課題　再提出

--- kadai_014/square.js
+++ kadai_014/square.js
@@ -1,7 +1,13 @@
+// JavaScriptの基礎を学ぼう_14章課題
+
 // 2乗は　「〇**2」と記述する。　「Math.pow」と同じ。
 
 const calcurateSquare = (square) => {
-  console.log( square ** 2 );
+  // 引数で渡した値を二乗　(関数の実行結果を返す)
+  return square ** 2;
 }
+// 関数calcurateSquareを呼び出し、引数3を渡す
+const result = calcurateSquare(3)
 
-console.log(calcurateSquare(3));
+// 戻り値を出力
+console.log(result);
